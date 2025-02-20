4db4a4b9c896824f41b5edf39b92a8b383c92c60
hallowslab <hallow.tech.lab@gmail.com>
2025-02-20 00:18:56 +0000

    fixes validation, allows windows path like "C:\..."

--- src/config.rs
+++ src/config.rs
@@ -36,8 +36,9 @@ pub fn validate_username(username: &str) -> Result<String, String> {
 pub fn validate_directory(directory: &str) -> Result<String, String> {
     // Statics cannot have dynamic memory allocations and need to be known at compile time,
     // define the arrays and respective sizes, we ignore "/" since the user can specify ./dir which is valid in Windows
+    // we also ignore / and : in case the user specifies a full path with Disk drive
     // Validation based on https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file
-    static INVALID_CHARS: [&str; 8] = ["<", ">", ":", "\"", "\\", "|", "?", "*"];
+    static INVALID_CHARS: [&str; 6] = ["<", ">", "\"", "|", "?", "*"];
     static INVALID_NAMES: [&str; 30] = ["CON", "PRN", "AUX", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5",
                                     "COM6", "COM7", "COM8", "COM9", "COM¹", "COM²", "COM³", "LPT0", "LPT1", "LPT2",
                                     "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9", "LPT¹", "LPT²", "LPT³"];
@@ -49,6 +50,10 @@ pub fn validate_directory(directory: &str) -> Result<String, String> {
     // Convert directory to uppercase to handle case insensitivity
     let directory_upper = directory.to_uppercase();
 
+    // TODO: Fix validation
+    // Should fail because it contains a reserved name, however this is incorrect I believe it should only fail
+    // with extensions like AUX.txt since it matches AUX
+    // https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file
     // Check if the directory name contains any reserved names as substrings
     if INVALID_NAMES.iter().any(|&reserved| directory_upper.contains(reserved)) {
         return Err(format!("Path {} contains a reserved name", directory));
