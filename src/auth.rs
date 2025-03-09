2651b1752a14df78932cdbfb57fada5fcaf488de
hallowslab <hallow.tech.lab@gmail.com>
2025-03-09 00:58:12 +0000

    Print logins

--- src/auth.rs
+++ src/auth.rs
@@ -16,9 +16,11 @@ impl Authenticator<DefaultUser> for StaticAuthenticator {
     ) -> Result<DefaultUser, AuthenticationError> {
         if let Some(password) = &credentials.password {  // Borrow credentials.password here because move occurs because `password` has type `std::string::String`, which does not implement the `Copy` trait
             if username == self.username && *password == self.password { // dereference password here because the trait `PartialEq<std::string::String>` is not implemented for `&std::string::String`
+                println!("Received valid login from {} for {}", credentials.source_ip, username);
                 return Ok(DefaultUser);
             }
         }
+        println!("Received invalid login from {} for {}", credentials.source_ip, username);
         Err(AuthenticationError::BadPassword)
     }
 }
