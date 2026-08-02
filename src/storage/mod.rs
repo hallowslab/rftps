1924f44ebd8034485d85f8058cd6664385ebd038
hallowslab <hallow.tech.lab@gmail.com>
2026-08-02 11:08:15 +0100

    add background job modules for FTPS and TLS utilities

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,3 +1,9 @@
 pub mod traits;
+#[cfg(feature = "background-jobs")]
+pub mod ftps;
+#[cfg(feature = "background-jobs")]
+pub mod tls_utils;
 
 pub use traits::{StorageBackend, StorageError};
+#[cfg(feature = "background-jobs")]
+pub use ftps::FtpsBackend;
